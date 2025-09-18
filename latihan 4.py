9bb76a68f745a24eaf663b2571b04c9e64ed8164
syamsul <151354144+MyBeClean@users.noreply.github.com>
2025-09-18 09:37:08 +0700

    Add files via upload

---
+++ latihan 4.py
@@ -0,0 +1,6 @@
+print('mancari luas segitiga : ')
+print('----------------------')
+alas = float(input('masukan alas : '))
+tinggi = float(input('masukan tinggi : '))
+luas = 0.5*alas*tinggi
+print('luasnya adalah : ', luas)
